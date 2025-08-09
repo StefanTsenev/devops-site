4fcb6a2a588221ccd1517435779f186fa734d97e
StefanTsenev <stefan.tsenev.edu@gmail.com>
2025-08-09 00:14:53 +0000

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@ set -Eeuo pipefail
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DEST_DIR="/var/www/html"
 
-# Синхронизирай проектните файлове към nginx docroot
-rsync -av --delete --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r "$SRC_DIR/" "$DEST_DIR/"
-
-# Релоуд/рестарт на nginx (ако не е нужен, може да се махне)
-systemctl reload nginx || systemctl restart nginx
+# Копирай само нужните файлове на сайта
+rsync -av --delete \
+  --exclude='.git' \
+  --exclude='.github' \
+  --exclude='deploy.sh' \
+  "$SRC_DIR/" "$DEST_DIR/"
 
 echo "Deploy OK"
